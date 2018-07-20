e9017048fa6f96236e793e32b8afd77df5d5e934
Veer Singh <veer.bhumca10@gmail.com>
2018-07-20 12:16:23 +0530

    Update ProductController.java

--- src/main/java/com/example/productapp/controller/ProductController.java
+++ src/main/java/com/example/productapp/controller/ProductController.java
@@ -21,7 +21,7 @@ class ProductController {
 	@GetMapping(path = "/products")
 	public String getProducts(Principal principal, Model model, HttpServletRequest request) {
 		ResponseEntity<String[]> response = restemplate.getForEntity(
-				"http://spring-boot-service1-spring-boot-product-app.azure.cloudmaf.io/products", String[].class);
+				"http://spring-boot-service1-spring-boot-product-demo.azure.cloudmaf.io/products", String[].class);
 
 		model.addAttribute("products", response.getBody());
 		model.addAttribute("principal", principal);
